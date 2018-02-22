838b1688779797f008da3e3695a5c17f4f19bd68
Ethel Annyaah <13200355@africa.airtel.itm>
2018-02-22 17:23:05 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slyder').slick({
+        dots:true,
+        autoplay:true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+  });
+      
